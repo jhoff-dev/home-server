dfa3e7385e9a579a0bc6c7758345771280816824
jhoff-dev <dev.jhoff@gmail.com>
2025-09-16 11:53:15 -0300

    Adjusts commands of containers

--- commands/container.mk
+++ commands/container.mk
@@ -1,13 +1,16 @@
 # Cloudflare Tunnels
-up:
+cloudflared-up:
 	sh -c "cd docker/cloudflare-tunnel && ${DOCKER_COMPOSE} up -d"
 
-destroy:
+cloudflared-destroy:
 	sh -c "cd docker/cloudflare-tunnel && ${DOCKER_COMPOSE} down --volumes"
 
 # Github Runner
-up:
+runner-up:
 	sh -c "cd docker/github-runner && ${DOCKER_COMPOSE} up -d"
 
-destroy:
+runner-destroy:
 	sh -c "cd docker/github-runner && ${DOCKER_COMPOSE} down --volumes"
+
+runner-build:
+	sh -c "export UID=$(shell id -u) && export GID=$(shell id -g) && export DOCKER_HOST_GID=$(shell getent group docker | cut -d: -f3) && docker compose build github-runner"
