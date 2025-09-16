1875c1bc895635f14ae6ccd558196d53e1789679
jhoff-dev <dev.jhoff@gmail.com>
2025-09-16 14:31:21 -0300

    Adjust runner github

--- commands/container.mk
+++ commands/container.mk
@@ -13,4 +13,4 @@ runner-destroy:
 	sh -c "cd docker/github-runner && ${DOCKER_COMPOSE} down --volumes"
 
 runner-build:
-	sh -c "export UID=$(shell id -u) && export GID=$(shell id -g) && export DOCKER_HOST_GID=$(shell getent group docker | cut -d: -f3) && docker compose build github-runner"
+	sh -c "cd docker/github-runner && export UID=$(shell id -u) && export GID=$(shell id -g) && export DOCKER_HOST_GID=$(shell getent group docker | cut -d: -f3) && docker compose build github-runner"
--- docker/github-runner/entrypoint.sh
+++ docker/github-runner/entrypoint.sh
@@ -12,4 +12,17 @@ if [ ! -f .runner ]; then
     --replace
 fi
 
+mkdir -p .ssh
+chmod 700 .ssh
+
+cat > .ssh/config <<EOF
+Host ssh.jhoff.site
+  ProxyCommand /usr/local/bin/cloudflared access ssh --hostname %h --id ${CLOUDFLARE_SSH_TUNNEL_ID} --secret ${CLOUDFLARE_SSH_TUNNEL_TOKEN} 
+  IdentityFile ~/.ssh/github-actions
+  StrictHostKeyChecking no
+  UserKnownHostsFile=/dev/null
+EOF
+
+chmod 600 .ssh/config
+
 exec ./run.sh
