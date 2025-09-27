9b4f95b3701d3901579a860287cb53269eee0a7e
jhoff-dev <dev.jhoff@gmail.com>
2025-09-27 17:09:04 -0300

    Add command to start all docker containers

--- commands/container.mk
+++ commands/container.mk
@@ -6,3 +6,11 @@ up:
 # Destroy the Docker services
 destroy:
 	sh -c "cd docker/$(path) && ${DOCKER_COMPOSE} down --volumes"
+
+# Start all server containers
+up-all:
+	make up path=cloudflare-tunnel
+	make up path=github-runner
+	make up path=web/nginx
+	make up path=web/otel
+	make up path=web/jhoff.site
